e522be5589bcbc706df00c88917f84cf3877eeca
ggulamtggul <68947445+ggulamtggul@users.noreply.github.com>
2025-03-03 15:05:25 +0900

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "2025년 04월 27일, 일요일 오후 01시 30분";
+export const WEDDING_LOCATION = "드메르 웨딩홀, 2층 CN홀";
+
+export const GROOM_NAME = "서광덕";
+export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_NAME = "○○○";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_NAME = "○○○";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+
+export const BRIDE_NAME = "박유민";
+export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_NAME = "○○○";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_NAME = "○○○";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
